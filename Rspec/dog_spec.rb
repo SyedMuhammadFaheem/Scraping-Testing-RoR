cc1fa80374fd4f5138c586705aa89642aaac28d4
SyedMuhammadFaheem <seyd.faheem.official@gmail.com>
2023-06-23 17:33:23 +0500

    using subject

--- Rspec/dog_spec.rb
+++ Rspec/dog_spec.rb
@@ -3,9 +3,9 @@ require_relative 'dog'
 
 describe Dog do
     describe '#bark' do
+        subject {Dog.new}
         it 'returns string "Woof!"' do
-            dog = Dog.new
-            expect(dog.bark).to eql('Woof!')
+            expect(subject.bark).to eql('Woof!')
         end
 
     end    
